e2e294483c7f74fcc32a2cac2a19400fd423a0ec
Whyaziz <rizkyaziz214@gmail.com>
2023-10-10 14:01:36 +0700

    feat : add tabel pemain

---
+++ resources/views/pemain/index.blade.php
@@ -0,0 +1,39 @@
+
+@extends('layouts.master')
+
+@section('title', 'Pemain')
+
+@section('content')
+    <h1 class="px-16 mt-8 mb-8 text-bold font-weight-bold text-[30px]">Pemain</h1>
+
+    
+<div class="relative overflow-x-auto px-16">
+    <table class="w-full text-sm text-left text-white p-8">
+        <thead class="text-xs text-gray-700 uppercase bg-red-200">
+        <tr>
+            <th scope="col">No</th>
+            <th scope="col">Nama</th>
+            <th scope="col">No Punggung</th>
+            <th scope="col">Posisi</th>
+        </tr>
+        </thead>
+
+        <tbody>
+            @php
+            $no = 0;
+            @endphp
+            @foreach ($pemain as $item)
+            <tr class="bg-red-500 border-b ">
+                <td class="px-6 py-4">{{ ++$no }}</td>
+                <td class="px-6 py-4">{{ $item->id }}</td>
+                <td class="px-6 py-4">{{ $item->nama_pemain }}</td>
+                <td class="px-6 py-4">{{ $item->no_punggung }}</td>
+                <td class="px-6 py-4">{{ $item->posisi }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+
+        
+    </table>
+</div>
+@endsection
